8a29013da9e91ec85b11681ce1c2655e944cb57b
hamilkarr <12uclock@gmail.com>
2022-03-23 08:21:37 +0900

    출근

--- ch15_miniProject/login_check.php
+++ ch15_miniProject/login_check.php
@@ -14,7 +14,7 @@ if (!$mb_id || !$mb_password) {
 // 해당 아이디가 존재하는지 체크
 $sql = "Select * from gnu_member Where mb_id = '$mb_id' ";
 $result = mysqli_query($conn,$sql); // 쿼리문을 사용해서 데이터베이스에서 아이디를 검색.
-$mb = myslqi_fetch_assoc($result); // 조회한 회원 정보의 결과를 $mb변수에 저장.
+$mb = mysqli_fetch_assoc($result); // 조회한 회원 정보의 결과를 $mb변수에 저장.
 
 // 입력한 비밀번호를 MySQL_password()함수를 이용해서 암호화 해서 가져옴.
 $sql = "Select PASSWORD('$mb_password') as Pass "; // PASSWORD 소문자도 가능한지 시도해볼것.
